fa81c1ecb56ed2bcb2f5b63d632ef578785d67af
Nightmare <datalab2026@gmail.com>
2026-07-29 02:03:56 +0300

    Create Config.js with bot and permissions settings
    Added configuration settings for bot and permissions.

---
+++ Config.js
@@ -0,0 +1,28 @@
+// Bot Information
+name = "SARU Bot"
+description = ""
+bot_token = ""
+clientID = ""
+// Permmisions Information 
+administration = ""
+human_resources = ""
+officer = ""
+// Discord Information
+serverID = ""
+// Channels IDs
+notifications = ""
+errors = ""
+rankings = ""
+joinings = ""
+// Stats Channels
+division_exp = ""
+division_level = ""
+hicom_members = ""
+officer_members = ""
+enlisted_members = ""
+// Roblox Information 
+groupID = ""
+NewbieID = ""
+// Extra External Tokens
+render_restart = ""
+google_sheets = ""
