e011a38d031bfa6ededfdbed6a1785e14fdd2029
Nightmare <datalab2026@gmail.com>
2026-08-01 18:47:45 +0300

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -1,1 +1,19 @@
+const http = require('http');
+
+// Создаем простой HTTP-сервер
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running!');
+});
+
+// Получаем порт из переменной окружения Render или используем 10000 по умолчанию
+const PORT = process.env.PORT || 10000;
+
+function keepAlive() {
+    server.listen(PORT, '0.0.0.0', () => {
+        console.log(`[KeepAlive]: Веб-сервер успешно запущен на порту ${PORT}`);
+    });
+}
+
+module.exports = { keepAlive };
 
