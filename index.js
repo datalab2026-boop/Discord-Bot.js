c51dd87387ab94c1da3f6da4f33c9f1f81f07558
Nightmare <datalab2026@gmail.com>
2026-08-01 18:46:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const config = require('./config.js');
 
 // 2. Подключаем модуль авторизации/подключения к Discord
 require('./connection.js');
+
+// 3. запуск веб сервера
+require('./web_server.js');
