20689c4a7a75af897b80e755762e0838549c8587
Nightmare <datalab2026@gmail.com>
2026-08-01 18:26:24 +0300

    Refactor config.js to use comma-separated format

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 import os
-// Bot Information
-name = "SARU Bot"
-description = ""
-bot_token: process.env["Discord_Token"]
-clientID: process.env["Client_ID"]
+// Bot Information,
+name = "SARU Bot",
+description = "",
+bot_token: process.env["Discord_Token"],
+clientID: process.env["Client_ID"],
 // Permmisions Information 
-administration = "1485230165830402168"
-human_resources = "1487518138974208142"
-officer = "1487394979478503565"
-head_moderator = "1477270727899222138"
-moderator = "1477270716050178169"
-division_administrator= "1485370389046759455"
+administration = "1485230165830402168",
+human_resources = "1487518138974208142",
+officer = "1487394979478503565",
+head_moderator = "1477270727899222138",
+moderator = "1477270716050178169",
+division_administrator= "1485370389046759455",
 // Discord Information
-serverID = "1457749135187906665"
+serverID = "1457749135187906665",
 // Channels IDs
-notifications = ""
-errors = ""
-rankings = ""
-joinings = ""
+notifications = "",
+errors = "",
+rankings = "",
+joinings = "",
 // Stats Channels
-division_exp = ""
-division_level = ""
-server_members = ""
-group_members = ""
-hicom_members = ""
-officer_members = ""
-enlisted_members = ""
+division_exp = "",
+division_level = "",
+server_members = "",
+group_members = "",
+hicom_members = "",
+officer_members = "",
+enlisted_members = "",
 // Roblox Information 
-groupID = ""
-newbieID = ""
+groupID = "",
+newbieID = "",
 // Extra External Tokens
-RENDER_RESTART_TOKEN = process.env["Restart_Token"]
-GOOGLE_SHEET_API = process.env["Sheet_API"]
+RENDER_RESTART_TOKEN = process.env["Restart_Token"],
+GOOGLE_SHEET_API = process.env["Sheet_API"],
 http_token = process.env["Http_Token"]
